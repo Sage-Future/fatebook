8b02069864cbc1406d4c8f3e1cd54c9197030d17
f-prettyland <fpriestland@gmail.com>
2023-03-22 14:24:23 +0000

    Removes event stuff since not using currently

--- api/events_handlers/_challenge.ts
+++
@@ -1,9 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export function challenge(req : VercelRequest, res: VercelResponse) {
-  console.log('req body challenge is:', req.body.challenge)
-
-  res.status(200).send({
-    challenge: req.body.challenge,
-  })
-}
