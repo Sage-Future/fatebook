99a8ced0946e21fa0f06078d73f74cca59d2887a
Adam Binks <adam.r.binks@gmail.com>
2023-04-27 18:17:52 +0100

    Add capacitor iOS target

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   eslint: {
     dirs: ['api', 'lib', 'pages', 'public', 'styles'],
   },
+  // Need this only for capacitor export (todo: control this with env var)
+  // images: {
+  //   unoptimized: true,
+  // }
 }
 
 export default nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import { GoogleAnalytics } from "nextjs-google-analytics"
 import Meta from "../components/Meta"
 import "../styles/globals.css"
 import { AppProps } from "next/app"
+import Head from "next/head"
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Meta />
+    <Head>
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
     <GoogleAnalytics trackPageViews />
     <Component {...pageProps} />
   </>
