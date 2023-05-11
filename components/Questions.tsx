8caf953edf21865db16dccb1e82cec09e4ad684a
Adam Binks <adam.r.binks@gmail.com>
2023-05-11 11:15:40 +0100

    questions page

--- components/Questions.tsx
+++ components/Questions.tsx
@@ -27,33 +27,35 @@ export function Questions() {
         {questions.data.sort(
           (a, b) => b.createdAt.getTime() - a.createdAt.getTime()
         ).map((question) => (
-          <div className="grid grid-cols-1 xl:grid-cols-2 gap-1" key={question.id}>
-            <span className="col-span-2 xl:col-span-1 font-semibold" key={`${question.id}title`}>
-              <Link href={`q/${question.id}`}>{question.title}</Link>
-            </span>
-            <div className="grid grid-cols-3">
-              <span className="text-sm" key={`${question.id}author`}>
-                <Username user={question.profile.user} />
+          <Link href={`q/${question.id}`} key={question.id}>
+            <div className="grid grid-cols-1 xl:grid-cols-2 gap-1" key={question.id}>
+              <span className="col-span-2 xl:col-span-1 font-semibold" key={`${question.id}title`}>
+                {question.title}
               </span>
-              {
-                question.resolvedAt ? (
-                  <span className="text-sm text-gray-400" key={`${question.id}resolve`}>
-                    <span>Resolved</span> <FormattedDate date={question.resolvedAt} />
-                  </span>
-                ) : (
-                  <span className={clsx(
-                    "text-sm text-gray-400",
-                    question.resolveBy < new Date() && "text-indigo-300"
-                  )} key={`${question.id}resolve`}>
-                    <span>Resolves</span> <FormattedDate date={question.resolveBy} />
-                  </span>
-                )
-              }
-              <span className="text-sm" key={`${question.id}resolution`}>
-                {question.resolution}
-              </span>
+              <div className="grid grid-cols-3">
+                <span className="text-sm" key={`${question.id}author`}>
+                  <Username user={question.profile.user} />
+                </span>
+                {
+                  question.resolvedAt ? (
+                    <span className="text-sm text-gray-400" key={`${question.id}resolve`}>
+                      <span>Resolved</span> <FormattedDate date={question.resolvedAt} />
+                    </span>
+                  ) : (
+                    <span className={clsx(
+                      "text-sm text-gray-400",
+                      question.resolveBy < new Date() && "text-indigo-300"
+                    )} key={`${question.id}resolve`}>
+                      <span>Resolves</span> <FormattedDate date={question.resolveBy} />
+                    </span>
+                  )
+                }
+                <span className="text-sm" key={`${question.id}resolution`}>
+                  {question.resolution}
+                </span>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
--- next.config.js
+++ next.config.js
@@ -5,9 +5,17 @@ const nextConfig = {
   eslint: {
     dirs: ['api', 'lib', 'pages', 'public', 'styles'],
   },
-  // Need this only for capacitor export (todo: control this with env var)
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.slack-edge.com',
+        port: '',
+        pathname: '**',
+      },
+      // Need this only for capacitor export (todo: control this with env var)
+      //   unoptimized: true,
+    ],
   }
 }
 
