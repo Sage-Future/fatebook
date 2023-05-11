a6484349fecfe77d3ea8eb50efbab80c0f2057fc
Adam Binks <adam.r.binks@gmail.com>
2023-05-11 12:23:38 +0100

    Fix typo

--- lib/blocks-designs/app_home.ts
+++ lib/blocks-designs/app_home.ts
@@ -107,7 +107,7 @@ export async function buildHomeTabBlocks(teamId: string, allUserForecasts: Forec
     {
       "type": "section",
       "text": markdownBlock(
-        '• You can now hide other forecaster’s predictions on a question to prevent anchoring. Look for the new option when you use `/forecast`\n' +
+        '• You can now hide other forecasters’ predictions on a question to prevent anchoring. Look for the new option when you use `/forecast`\n' +
         `• Create private forecasts by <slack://app?team=${teamId}&id=${slackAppId}&tab=messages|DMing @Fatebook> - just type \`/forecast\``
       )
     },
