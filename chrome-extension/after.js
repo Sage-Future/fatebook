87a6ebc9dfab2f7df82edf6cecba29c8fad20352
Jarred Filmer <jarred.filmer@gmail.com>
2023-07-28 13:53:44 +1000

    merge

--- chrome-extension/after.js
+++ chrome-extension/after.js
@@ -1,151 +1,171 @@
 // this code will be executed after page load
-;(function () {
-   console.log("after.js executed")
+; (function () {
 
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log(sender.tab ?
+      "from a content script:" + sender.tab.url :
+      "from the extension");
 
-    document.body.addEventListener('keydown', (event) => {
-    console.log("eeeeeyo")
-    
-    if(event.key === '`') {
-      showPredictionModal()
+    if (request.action === "open_modal") {
+      openModal();
+    } else {
+      throw new Error(`unknown action "${request.action}"`)
     }
-  })
-  
-  function showPredictionModal() {
-    alert('ey!')
+  });
+
+  const iframe = document.createElement('iframe')
+  iframe.className = 'fatebook-predict-embed'
+  iframe.src = 'http://localhost:3000/embed/predict-modal'
+  iframe.style.display = 'none'
+  document.body.appendChild(iframe);
+
+  function openModal() {
+    iframe.style.display = 'block'
+    iframe.focus()
+    iframe.contentWindow?.postMessage({ isFatebook: true, action: 'focus' }, '*')
+
+    window.addEventListener('message', (event) => {
+      if (typeof event.data === 'object' && event.data.isFatebook) {
+        iframe.style.display = 'none'
+      }
+    })
   }
 
-   let commentParent
-   const intervalComments = setInterval(() => {
-      commentParent = document.querySelector(".docos-stream-view")
-      if (!commentParent) return
-      else {
-         clearInterval(intervalComments)
+  // openModal()
 
-         // Options for the observer (which mutations to observe)
-         const config = { attributes: true, childList: true, subtree: true }
 
-         // Callback function to execute when mutations are observed
-         const callback = (mutationList, observer) => {
-            const comments = commentParent.querySelectorAll(".docos-replyview-body")
 
-            for (const comment of comments) {
-               const link = comment.querySelector("a")
-               if (!link) continue
+  let commentParent
+  const intervalComments = setInterval(() => {
+    commentParent = document.querySelector(".docos-stream-view")
+    if (!commentParent) return
+    else {
+      clearInterval(intervalComments)
 
-               const ourLink = link.href.includes("example.com")
+      // Options for the observer (which mutations to observe)
+      const config = { attributes: true, childList: true, subtree: true }
 
-               const ui = comment.querySelector("#fatebook-ui")
-               const uiIsInjected = !!ui
+      // Callback function to execute when mutations are observed
+      const callback = (mutationList, observer) => {
+        const comments = commentParent.querySelectorAll(".docos-replyview-body")
 
-               if (uiIsInjected && !ourLink) {
-                  console.log("remove")
-                  ui.remove()
-               } else if (!uiIsInjected && ourLink) {
-                  console.log("append")
+        for (const comment of comments) {
+          const link = comment.querySelector("a")
+          if (!link) continue
 
-                  const div = document.createElement("iframe")
-                  div.id = "fatebook-ui"
-                  div.src = "https://fatebook.io/"
-                  div.style.zoom = ".1"
-                  div.style.width = "100%"
-                  div.style.height = "2000px"
+          const ourLink = link.href.includes("example.com")
 
-                  comment.appendChild(div)
-               }
-            }
-         }
+          const ui = comment.querySelector("#fatebook-ui")
+          const uiIsInjected = !!ui
 
-         // Create an observer instance linked to the callback function
-         const observer = new MutationObserver(callback)
-         observer.observe(commentParent, config)
+          if (uiIsInjected && !ourLink) {
+            console.log("remove")
+            ui.remove()
+          } else if (!uiIsInjected && ourLink) {
+            console.log("append")
+
+            const div = document.createElement("iframe")
+            div.id = "fatebook-ui"
+            div.src = "https://fatebook.io/"
+            div.style.zoom = ".1"
+            div.style.width = "100%"
+            div.style.height = "2000px"
+
+            comment.appendChild(div)
+          }
+        }
       }
-   }, 1000)
 
-   let popupParent
-   const intervalPopup = setInterval(() => {
-      popupParent = document.querySelector(".docs-linkbubble-bubble")
-      if (!popupParent) return
-      else {
-         clearInterval(intervalPopup)
+      // Create an observer instance linked to the callback function
+      const observer = new MutationObserver(callback)
+      observer.observe(commentParent, config)
+    }
+  }, 1000)
 
-         // Options for the observer (which mutations to observe)
-         const config = { attributes: true, childList: true, subtree: true }
+  let popupParent
+  const intervalPopup = setInterval(() => {
+    popupParent = document.querySelector(".docs-linkbubble-bubble")
+    if (!popupParent) return
+    else {
+      clearInterval(intervalPopup)
 
-         // Callback function to execute when mutations are observed
-         const callback = (mutationList, observer) => {
-            const href = popupParent.querySelector("a").href
-            const ourLink = href.includes("example.com")
+      // Options for the observer (which mutations to observe)
+      const config = { attributes: true, childList: true, subtree: true }
 
-            const ui = popupParent.querySelector("#fatebook-ui")
-            const uiIsInjected = !!ui
+      // Callback function to execute when mutations are observed
+      const callback = (mutationList, observer) => {
+        const href = popupParent.querySelector("a").href
+        const ourLink = href.includes("example.com")
 
-            if (uiIsInjected && !ourLink) {
-               console.log("remove")
-               ui.remove()
-            } else if (!uiIsInjected && ourLink) {
-               console.log("append")
-               const div = document.createElement("iframe")
-               div.id = "fatebook-ui"
-               div.src = "https://fatebook.io/"
-               div.style.zoom = ".1"
-               div.style.width = "100%"
-               div.style.height = "2000px"
+        const ui = popupParent.querySelector("#fatebook-ui")
+        const uiIsInjected = !!ui
 
-               popupParent.appendChild(div)
-            }
-         }
+        if (uiIsInjected && !ourLink) {
+          console.log("remove")
+          ui.remove()
+        } else if (!uiIsInjected && ourLink) {
+          console.log("append")
+          const div = document.createElement("iframe")
+          div.id = "fatebook-ui"
+          div.src = "https://fatebook.io/"
+          div.style.zoom = ".1"
+          div.style.width = "100%"
+          div.style.height = "2000px"
 
-         // Create an observer instance linked to the callback function
-         const observer = new MutationObserver(callback)
-         observer.observe(popupParent, config)
+          popupParent.appendChild(div)
+        }
       }
-   }, 1000)
 
-   let commentTray
-   const commentTrayInterval = setInterval(() => {
-      commentTray = document.querySelector(".docs-instant-docos-content")
-      if (!commentTray) return
-      else {
-         clearInterval(commentTrayInterval)
+      // Create an observer instance linked to the callback function
+      const observer = new MutationObserver(callback)
+      observer.observe(popupParent, config)
+    }
+  }, 1000)
 
-         const div = document.createElement("div")
-         div.innerText = "ADD PREDICTION"
+  let commentTray
+  const commentTrayInterval = setInterval(() => {
+    commentTray = document.querySelector(".docs-instant-docos-content")
+    if (!commentTray) return
+    else {
+      clearInterval(commentTrayInterval)
 
-         
+      const div = document.createElement("div")
+      div.innerText = "ADD PREDICTION"
 
-         commentTray.appendChild(div)
 
-         // // Options for the observer (which mutations to observe)
-         // const config = { attributes: true, childList: true, subtree: true }
 
-         // // Callback function to execute when mutations are observed
-         // const callback = (mutationList, observer) => {
-         //    const href = popupParent.querySelector("a").href
-         //    const ourLink = href.includes("example.com")
+      commentTray.appendChild(div)
 
-         //    const ui = popupParent.querySelector("#fatebook-ui")
-         //    const uiIsInjected = !!ui
+      // // Options for the observer (which mutations to observe)
+      // const config = { attributes: true, childList: true, subtree: true }
 
-         //    if (uiIsInjected && !ourLink) {
-         //       console.log("remove")
-         //       ui.remove()
-         //    } else if (!uiIsInjected && ourLink) {
-         //       console.log("append")
-         //       const div = document.createElement("iframe")
-         //       div.id = "fatebook-ui"
-         //       div.src = "https://fatebook.io/"
-         //       div.style.zoom = ".1"
-         //       div.style.width = "100%"
-         //       div.style.height = "2000px"
+      // // Callback function to execute when mutations are observed
+      // const callback = (mutationList, observer) => {
+      //    const href = popupParent.querySelector("a").href
+      //    const ourLink = href.includes("example.com")
 
-         //       popupParent.appendChild(div)
-         //    }
-         // }
+      //    const ui = popupParent.querySelector("#fatebook-ui")
+      //    const uiIsInjected = !!ui
 
-         // // Create an observer instance linked to the callback function
-         // const observer = new MutationObserver(callback)
-         // observer.observe(popupParent, config)
-      }
-   }, 1000)
+      //    if (uiIsInjected && !ourLink) {
+      //       console.log("remove")
+      //       ui.remove()
+      //    } else if (!uiIsInjected && ourLink) {
+      //       console.log("append")
+      //       const div = document.createElement("iframe")
+      //       div.id = "fatebook-ui"
+      //       div.src = "https://fatebook.io/"
+      //       div.style.zoom = ".1"
+      //       div.style.width = "100%"
+      //       div.style.height = "2000px"
+
+      //       popupParent.appendChild(div)
+      //    }
+      // }
+
+      // // Create an observer instance linked to the callback function
+      // const observer = new MutationObserver(callback)
+      // observer.observe(popupParent, config)
+    }
+  }, 1000)
 })()
--- chrome-extension/before.js
+++ chrome-extension/before.js
@@ -2,21 +2,5 @@
 ; (function () {
   console.log("before.js executed")
 
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log(sender.tab ?
-      "from a content script:" + sender.tab.url :
-      "from the extension");
 
-    if (request.action === "open_modal") {
-      openModal();
-    } else {
-      throw new Error(`unknown action "${request.action}"`)
-    }
-  });
-
-
-  function openModal() {
-    alert('eyo!')
-    // todo, put http://localhost:3000/embed/predict in an iframe and show as a modal
-  }
 })()
