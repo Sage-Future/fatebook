f108a7729b3539f8a94bf59c4afbd481f7f2662e
Jarred Filmer <jarred.filmer@gmail.com>
2023-08-07 12:15:40 +1000

    remove cors wildcards

--- next.config.js
+++ next.config.js
@@ -16,20 +16,6 @@ const nextConfig = {
       // Need this only for capacitor export (todo: control this with env var)
       //   unoptimized: true,
     ],
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
   }
 }
 
