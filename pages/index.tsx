1d676fd7eefd418290b80529375757bb645118f4
Adam Binks <adam.r.binks@gmail.com>
2023-08-22 12:20:41 +0100

    Add logged out landing page copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { ChatBubbleOvalLeftIcon, RocketLaunchIcon, TrophyIcon } from '@heroicons/react/24/solid'
 import { useSession } from "next-auth/react"
 import { Predict } from "../components/Predict"
 import { Questions } from "../components/Questions"
@@ -16,10 +17,27 @@ export default function HomePage() {
         <Predict />
         <Questions />
 
+        {
+          !session?.user?.id && <SignedOutInfo />
+        }
+
       </div>
       <div className="pt-28 lg:w-[320px] max-sm:hidden">
         <TrackRecord />
       </div>
+    </div>
+  )
+}
+
+function SignedOutInfo() {
+  return (
+    <div className="my-6">
+      <h2>{"Why build a habit of forecasting?"}</h2>
+      <ul className="list-none space-y-4 pl-0">
+        <li className="flex items-center space-x-3"><RocketLaunchIcon className="flex-shrink-0 mr-2 w-6 h-6 text-indigo-500 inline-block" /><span><span className='font-semibold'>{"Make better decisions"}</span><br/>Get a clearer view of the consequences by thinking about the important questions.</span></li>
+        <li className="flex items-center space-x-3"><ChatBubbleOvalLeftIcon className="flex-shrink-0 mr-2 w-6 h-6 text-indigo-500 inline-block" /><span><span className='font-semibold'>{"Communicate more clearly"}</span><br/>{"Write down your prediction as a probability. 'Probably' is ambiguous, '80%' isn't."}</span></li>
+        <li className="flex items-center space-x-3"><TrophyIcon className="flex-shrink-0 mr-2 w-6 h-6 text-indigo-500 inline-block" /><span><span className='font-semibold'>{"Build your track record"}</span><br/>Resolve your predictions as YES, NO or AMBIGUOUS. A feedback loop to develop powerful habits of mind.</span></li>
+      </ul>
     </div>
   )
 }
