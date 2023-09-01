100e75cfcb65a0021ed1f52e7754783c064b7d0d
Jarred Filmer <jarred.filmer@gmail.com>
2023-09-01 14:42:34 +0100

    sentry

--- next.config.js
+++ next.config.js
@@ -39,6 +39,8 @@ const sentryWebpackPluginOptions = {
   project: "fatebook",
   silent: true, // Suppresses all logs
 
+  configFile:"sentry.client.config.cjs"
+
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
