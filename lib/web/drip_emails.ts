f1c1f2fae4a16a2da7e567bcb79c119727635205
Adam Binks <adam.r.binks@gmail.com>
2024-03-07 09:28:38 +0000

    fix welcome email

--- lib/web/drip_emails.ts
+++ lib/web/drip_emails.ts
@@ -7,14 +7,14 @@ export async function sendWelcomeEmail(user: User) {
     subject: "Welcome to Fatebook",
     htmlBody: `
     <p>Hi${" " + user.name || ""},</p>
-    <p>Welcome to <a href="https://fatebook.io>Fatebook</a>: the fastest way to make and track predictions!</p>
+    <p>Welcome to <a href="https://fatebook.io">Fatebook</a>: the fastest way to make and track predictions!</p>
     <p>Here are some things you could try using Fatebook for:</p>
     <ul>
       <li>Predict whether you'll complete your weekly goals in your weekly review. If the probability is low, consider ways to raise it!</li>
       <li>When your team starts a new project, forecast key assumptions and outcomes.</li>
       <li>Consider: what's the most important decision in your life right now, and how can breaking it down into concrete predictions help you make a good call?</li>
     </ul>
-    <p>You can use Fatebook on <a href="https://fatebook.io">fatebook.io</a>, in <a href="https://fatebook.io/for-slack">Slack</a>, inside Google Docs or anywhere on the web with the <a href="https://fatebook.io/extension">browser extension</a>, or via the <a href="https://fatebook.io/api">API</a>.</p>
+    <p>You can use Fatebook on <a href="https://fatebook.io">fatebook.io</a>, in <a href="https://fatebook.io/for-slack">Slack</a>, inside Google Docs, or anywhere on the web with the <a href="https://fatebook.io/extension">browser extension</a>, or via the <a href="https://fatebook.io/api">API</a>.</p>
     <p>You might also be interested in our forecasting training tools, <a href="https://quantifiedintuitions.org">Quantified Intuitions</a>.</p>
     <p>I want to make Fatebook as useful as possible, so I'd love to hear from you! What brought you here, and what would you like to get out of Fatebook?</p>
     <p>Thanks,<br/>Adam</p>
