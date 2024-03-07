1b8ca144903634c1b210fcc686e990a50bca76f6
Adam Binks <adam.r.binks@gmail.com>
2024-03-07 09:31:15 +0000

    fix email

--- lib/web/drip_emails.ts
+++ lib/web/drip_emails.ts
@@ -14,7 +14,7 @@ export async function sendWelcomeEmail(user: User) {
       <li>When your team starts a new project, forecast key assumptions and outcomes.</li>
       <li>Consider: what's the most important decision in your life right now, and how can breaking it down into concrete predictions help you make a good call?</li>
     </ul>
-    <p>You can use Fatebook on <a href="https://fatebook.io">fatebook.io</a>, in <a href="https://fatebook.io/for-slack">Slack</a>, inside Google Docs, or anywhere on the web with the <a href="https://fatebook.io/extension">browser extension</a>, or via the <a href="https://fatebook.io/api">API</a>.</p>
+    <p>You can use Fatebook on <a href="https://fatebook.io">fatebook.io</a>, in <a href="https://fatebook.io/for-slack">Slack</a>, inside Google Docs, anywhere on the web with the <a href="https://fatebook.io/extension">browser extension</a>, or via the <a href="https://fatebook.io/api">API</a>.</p>
     <p>You might also be interested in our forecasting training tools, <a href="https://quantifiedintuitions.org">Quantified Intuitions</a>.</p>
     <p>I want to make Fatebook as useful as possible, so I'd love to hear from you! What brought you here, and what would you like to get out of Fatebook?</p>
     <p>Thanks,<br/>Adam</p>
@@ -24,20 +24,20 @@ export async function sendWelcomeEmail(user: User) {
       false,
     )}">here</a>.)</i></small></p>
   `,
-    textBody: `Hi ${user.name},
+    textBody: `Hi${user.name ? ` ${user.name}` : ""},
 
 Welcome to Fatebook: the fastest way to make and track predictions.
 
-Here are some things people use Fatebook for:
-- Predict which of your weekly goals you'll complete in your weekly review. If the probability is low, consider ways to raise it!
-- When your team starts a new project, make forecasts on key assumptions and outcomes.
+Here are some things you could try using Fatebook for:
+- Predict whether you'll complete your weekly goals in your weekly review. If the probability is low, consider ways to raise it!
+- When your team starts a new project, forecast key assumptions and outcomes.
 - Consider: what's the most important decision in your life right now, and how can breaking it down into concrete predictions help you make a good call?
 
 You can use Fatebook on fatebook.io, in Slack, anywhere on the web with the browser extension, or via the API.
 
 You might also be interested in our forecasting training tools, Quantified Intuitions.
 
-I want to make Fatebook as useful as possible, so I'd love to hear from you. What brought you here, and what would you like to get out of Fatebook?
+I want to make Fatebook as useful as possible, so I'd love to hear from you! What brought you here, and what would you like to get out of Fatebook?
 
 Thanks,
 Adam
