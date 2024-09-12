58ba9e7bdb64f30ea7e069d81f0825af4e941adb
Jonny Spicer <spicerjonny@gmail.com>
2024-09-12 12:37:06 +0100

    feat: adds basic draw functionality to /teams bottom nav

--- pages/teams.tsx
+++ pages/teams.tsx
@@ -8,12 +8,13 @@ import { TeamListContent } from "../components/TeamListContent";
 import { TournamentContent } from "../components/TournamentContent";
 import { signInToFatebook } from "../lib/web/utils";
 // import { useSwipeable } from "react-swipeable";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import { ChevronLeftIcon, ChevronRightIcon, ArrowDownCircleIcon, ArrowUpCircleIcon, ChevronDoubleDownIcon, ChevronDoubleUpIcon } from "@heroicons/react/20/solid";
 
 export default function TeamsPage() {
   const userId = useUserId();
   const [currentIndex, setCurrentIndex] = useState(0);
   const containerRef = useRef<HTMLDivElement>(null);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const tournamentsQ = api.tournament.getAll.useQuery({});
   const userListsQ = api.userList.getUserLists.useQuery();
@@ -65,6 +66,9 @@ export default function TeamsPage() {
   //   delta: 50,
   //   swipeDuration: 500,
   // });
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
 
   useEffect(() => {
     const container = containerRef.current;
@@ -119,24 +123,40 @@ export default function TeamsPage() {
         )}
       </div>
       {userId && (
-        <div className="fixed bottom-16 left-0 right-0 bg-white border-t border-gray-200 p-4">
-          <div className="flex justify-between items-center max-w-6xl mx-auto">
+        <div className={`fixed bottom-16 lg:bottom-[100px] left-0 right-0 bg-white border-t border-gray-200 transition-all duration-300 z-20 ${isDrawerOpen ? 'h-[80vh]' : 'h-16'}`}>
+          <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
             <button 
               onClick={handlePrevious}
               className="btn text-primary"
             >
-              <ChevronLeftIcon className="w-4 h-4" />
+              <ChevronLeftIcon className="w-6 h-6" />
             </button>
-            <span className="text-lg font-semibold text-center mx-2 truncate" style={{ maxWidth: 'calc(100% - 200px)' }}>
-              {truncateName(currentItem.name, 30)}
-            </span>
+            <button 
+              onClick={toggleDrawer}
+              className="flex items-center justify-center flex-grow"
+            >
+              <span className="text-lg font-semibold text-center truncate">
+                {truncateName(currentItem.name, 30)}
+              </span>
+              {isDrawerOpen ? (
+                <ChevronDoubleDownIcon className="w-6 h-6 text-primary" />
+              ) : (
+                <ChevronDoubleUpIcon className="w-6 h-6 text-primary" />
+              )}
+            </button>
             <button 
               onClick={handleNext}
               className="btn text-primary"
             >
-              <ChevronRightIcon className="w-4 h-4" />
+              <ChevronRightIcon className="w-6 h-6" />
             </button>
           </div>
+          {isDrawerOpen && (
+            <div className="p-4 overflow-y-auto h-[calc(100%-4rem)]">
+              <Tournaments />
+              <UserLists />
+            </div>
+          )}
         </div>
       )}
     </div>
