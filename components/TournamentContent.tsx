f6905b8eabcb6079a8d3df91df8db7cf4ec96c5e
Jonny Spicer <spicerjonny@gmail.com>
2024-09-12 10:53:00 +0100

    fix: cleans up styling and content in TournamentContent component

--- components/TournamentContent.tsx
+++ components/TournamentContent.tsx
@@ -1,14 +1,12 @@
 import { Username } from "./ui/Username";
-import { SyncToSlack } from "./SyncToSlack";
 import { Questions } from "./Questions";
-import { getTournamentUrl } from "../lib/web/utils";
 import { TournamentWithAuthor } from "../prisma/additional";
 
 export function TournamentContent({ tournament, userId }: { tournament: TournamentWithAuthor, userId?: string }) {
   return (
     <div className="flex flex-col gap-2">
       <h2 className="mt-0">{tournament.name}</h2>
-      <p>{tournament.description}</p>
+      {tournament.description && <p>{tournament.description}</p>}
       <div>
         <span>Created by: </span>
         <Username
@@ -16,16 +14,7 @@ export function TournamentContent({ tournament, userId }: { tournament: Tourname
           className="bg-white px-2 py-1.5 rounded-full outline outline-1 outline-neutral-200 hover:bg-neutral-100"
         />
       </div>
-      {userId && tournament.authorId === userId && (
-        <SyncToSlack
-          listType="tournament"
-          url={getTournamentUrl(tournament, false)}
-          entity={tournament}
-        />
-      )}
-      <h3>Tournament Questions</h3>
       <Questions
-        title={"Tournament questions"}
         noQuestionsText="No questions in this tournament yet."
         filterTournamentId={tournament.id}
       />
