c961cd3fa7b37cf0e5b9558be15d976141d07e47
Jonny Spicer <spicerjonny@gmail.com>
2024-11-12 15:53:48 +0000

    fix: sets ResolveButton MenuItems to be static with no modal

--- components/questions/ResolveButton.tsx
+++ components/questions/ResolveButton.tsx
@@ -118,7 +118,11 @@ export function ResolveButton({
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute z-40 right-0 mt-2 w-40 origin-top-right divide-y divide-neutral-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items
+            className="absolute z-40 right-0 mt-2 w-40 origin-top-right divide-y divide-neutral-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            static
+            modal={false}
+          >
             <div className="px-1 py-1 " onClick={(e) => e.stopPropagation()}>
               {question.resolution || resolution ? (
                 <Menu.Item key={resolution}>
