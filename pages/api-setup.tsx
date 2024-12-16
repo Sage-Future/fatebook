1b96cd27005edbfaaf3db339eb3c08170e198081
Adam Binksmith <adam.r.binks@gmail.com>
2024-12-16 16:38:51 +0000

    improve api page copy

--- pages/api-setup.tsx
+++ pages/api-setup.tsx
@@ -1,3 +1,6 @@
+import { Disclosure, DisclosurePanel } from "@headlessui/react"
+import { ChevronDownIcon } from "@heroicons/react/20/solid"
+import clsx from "clsx"
 import { NextSeo } from "next-seo"
 import dynamic from "next/dynamic"
 import Link from "next/link"
@@ -24,15 +27,26 @@ export default function ApiPage() {
     <div className="px-4 pt-12 lg:pt-16 mx-auto max-w-6xl">
       <NextSeo
         title="API"
-        description="Use the API to create Fatebook questions from a URL"
+        description="Use the API to create, get, and resolve Fatebook questions from other applications"
       />
       <div className="mx-auto prose">
         <h2>Fatebook API</h2>
-        <h3>Use the API to create Fatebook questions from a URL</h3>
+        <p>
+          You can use the API to create, get, and resolve Fatebook questions
+          from other applications.
+        </p>
+        <p>
+          Get help on using the API in our{" "}
+          <Link href="https://discord.gg/mt9YVB8VDE" target="_blank">
+            Discord
+          </Link>
+          . You might also be interested in <Link href="/embed">embedding</Link>{" "}
+          Fatebook questions in your own website .
+        </p>
         {!userId && (
           <div className="flex w-full p-4">
             <button
-              className="button primary mx-auto"
+              className="btn btn-lg btn-primary mx-auto"
               onClick={() => void signInToFatebook()}
             >
               Sign in to set up your API key
@@ -91,136 +105,123 @@ export default function ApiPage() {
         </div>
         <h3>API Versions</h3>
         <p>
-          Fatebook offers two versions of the API: <code>v0</code> and{" "}
-          <code>v1</code>. New users should use the <code>v1</code> API, which
-          offers improved functionality and stability. The <code>v0</code>{" "}
-          documentation is preserved here for users who have already adopted
-          that version.
+          The latest version of the API is <code>v1</code>. New users should use
+          the <code>v1</code> API, which offers improved functionality and
+          stability. The <code>v0</code> documentation is preserved here for
+          users who have already adopted that version.
         </p>
 
-        <h4>V1 API (Recommended for new users)</h4>
-        <p>
-          The latest V1 API documentation is available below on this page. You
-          can find detailed information about endpoints and usage in the
-          <Link
-            href="#operations-tag-v1"
-            className="text-blue-600 hover:underline ml-1"
-          >
-            V1 API Documentation section
-          </Link>
-          .
-        </p>
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-100 px-4 py-2 text-left text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-gray-500 focus-visible:ring-opacity-75">
+                <span>Examples of specific v0 endpoints (legacy)</span>
+                <ChevronDownIcon
+                  className={clsx(
+                    "h-5 w-5 text-gray-500",
+                    open && "rotate-180 transform",
+                  )}
+                />
+              </Disclosure.Button>
+              <DisclosurePanel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                <b>GET /v0/createQuestion</b>
 
-        <h4>V0 API (Legacy)</h4>
-        <p>
-          The following documentation is for the <code>v0</code> API. If you
-          {"'"}re already using this version, you can continue to do so, but we
-          recommend migrating to <code>v1</code> for new integrations.
-        </p>
+                <p>
+                  You can use your API key to create Fatebook questions just by
+                  going this URL (separated onto multiple lines for
+                  readability):
+                </p>
 
-        <p>
-          Below are examples of two common endpoints. For a complete list of
-          available endpoints and detailed documentation for the v0 API, please
-          refer to the
-          <Link
-            href="#operations-tag-v0"
-            className="text-blue-600 hover:underline ml-1"
-          >
-            V0 API Documentation section
-          </Link>{" "}
-          in the OpenAPI specification further down this page.
-        </p>
+                <p className="bg-neutral-100 outline outline-neutral-300 outline-1 p-2 rounded-md break-words">
+                  {"https://fatebook.io/api/v0/createQuestion"}
+                  {"?apiKey="}
+                  {apiKey.data || "YOUR_API_KEY"}
+                  <br />
+                  {"&title="}
+                  <span className="bg-indigo-100 text-indigo-800 font-semibold">
+                    {"YOUR_QUESTION_TITLE"}
+                  </span>
+                  <br />
+                  {"&resolveBy="}
+                  <span className="bg-indigo-100 text-indigo-800 font-semibold">
+                    {"RESOLUTION_DATE_YYYY-MM-DD"}
+                  </span>
+                  <br />
+                  {"&forecast="}
+                  <span className="bg-indigo-100 text-indigo-800 font-semibold">
+                    {"FORECAST_BETWEEN_0_AND_1"}
+                  </span>
+                  <br />
+                </p>
 
-        <b>GET /v0/createQuestion</b>
+                <p>
+                  You can use this to integrate Fatebook with other tools, like
+                  iOS shortcuts. If you create an integration, let us know and
+                  we can tell other Fatebook users about it!
+                </p>
 
-        <p>
-          You can use your API key to create Fatebook questions just by going
-          this URL (separated onto multiple lines for readability):
-        </p>
+                <p>
+                  Having trouble? Ask for help in{" "}
+                  <Link href="https://discord.gg/mt9YVB8VDE" target="_blank">
+                    Discord
+                  </Link>
+                  .
+                </p>
 
-        <p className="bg-neutral-100 outline outline-neutral-300 outline-1 p-2 rounded-md break-words">
-          {"https://fatebook.io/api/v0/createQuestion"}
-          {"?apiKey="}
-          {apiKey.data || "YOUR_API_KEY"}
-          <br />
-          {"&title="}
-          <span className="bg-indigo-100 text-indigo-800 font-semibold">
-            {"YOUR_QUESTION_TITLE"}
-          </span>
-          <br />
-          {"&resolveBy="}
-          <span className="bg-indigo-100 text-indigo-800 font-semibold">
-            {"RESOLUTION_DATE_YYYY-MM-DD"}
-          </span>
-          <br />
-          {"&forecast="}
-          <span className="bg-indigo-100 text-indigo-800 font-semibold">
-            {"FORECAST_BETWEEN_0_AND_1"}
-          </span>
-          <br />
-        </p>
+                <b>GET /v0/getQuestion</b>
+                <p>
+                  {
+                    "You can also use the API to get Fatebook questions by ID, here's an example:"
+                  }
+                </p>
 
-        <p>
-          You can use this to integrate Fatebook with other tools, like iOS
-          shortcuts. If you create an integration, let us know and we can tell
-          other Fatebook users about it!
-        </p>
+                <p className="bg-neutral-100 outline outline-neutral-300 outline-1 p-2 rounded-md break-words">
+                  {"https://fatebook.io/api/v0/getQuestion"}
+                  {"?apiKey="}
+                  {apiKey.data || "YOUR_API_KEY"}
+                  <br />
+                  {"&questionId="}
+                  <span className="bg-indigo-100 text-indigo-800 font-semibold">
+                    {"QUESTION_ID"}
+                  </span>
+                  <br />
+                </p>
 
-        <p>
-          Having trouble? Ask for help in{" "}
-          <Link href="https://discord.gg/mt9YVB8VDE" target="_blank">
-            Discord
-          </Link>
-          .
-        </p>
+                <p>
+                  {
+                    "To get the question ID, go to the question page and copy the ID from the URL. The ID is the part after the --, e.g. for this question:"
+                  }{" "}
+                  <span className="bg-neutral-100 outline outline-neutral-300 outline-1 p-0.5 rounded-md">
+                    https://fatebook.io/q/will-adam-win-the-next-aoe-game---clkqtczp00001l008qcrma6s7
+                  </span>{" "}
+                  the ID is{" "}
+                  <span className="bg-neutral-100 outline outline-neutral-300 outline-1 p-0.5 rounded-md">
+                    {"clkqtczp00001l008qcrma6s7"}
+                  </span>
+                  .
+                </p>
 
-        <b>GET /v0/getQuestion</b>
-        <p>
-          {
-            "You can also use the API toget Fatebook questions by ID, here's an example:"
-          }
-        </p>
+                <p>
+                  Click{" "}
+                  <a
+                    href={`https://fatebook.io/api/v0/getQuestion?apiKey=${
+                      apiKey.data || "YOUR_API_KEY"
+                    }&questionId=clkqtczp00001l008qcrma6s7`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    here
+                  </a>{" "}
+                  to see an example of the getQuestion endpoint in action.
+                </p>
+              </DisclosurePanel>
+            </>
+          )}
+        </Disclosure>
 
-        <p className="bg-neutral-100 outline outline-neutral-300 outline-1 p-2 rounded-md break-words">
-          {"https://fatebook.io/api/v0/getQuestion"}
-          {"?apiKey="}
-          {apiKey.data || "YOUR_API_KEY"}
-          <br />
-          {"&questionId="}
-          <span className="bg-indigo-100 text-indigo-800 font-semibold">
-            {"QUESTION_ID"}
-          </span>
-          <br />
-        </p>
-
-        <p>
-          {
-            "To get the question ID, go to the question page and copy the ID from the URL. The ID is the part after the --, e.g. for this question:"
-          }{" "}
-          <span className="bg-neutral-100 outline outline-neutral-300 outline-1 p-0.5 rounded-md">
-            https://fatebook.io/q/will-adam-win-the-next-aoe-game---clkqtczp00001l008qcrma6s7
-          </span>{" "}
-          the ID is{" "}
-          <span className="bg-neutral-100 outline outline-neutral-300 outline-1 p-0.5 rounded-md">
-            {"clkqtczp00001l008qcrma6s7"}
-          </span>
-          .
-        </p>
-
-        <p>
-          Click{" "}
-          <a
-            href={`https://fatebook.io/api/v0/getQuestion?apiKey=${
-              apiKey.data || "YOUR_API_KEY"
-            }&questionId=clkqtczp00001l008qcrma6s7`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            here
-          </a>{" "}
-          to see an example of the getQuestion endpoint in action.
-        </p>
-        <h3>Integrations that other Fatebook users have created:</h3>
+        <h3>
+          Integrations that other Fatebook users have created using the API
+        </h3>
         <ul>
           <li>
             <Link href="https://www.icloud.com/shortcuts/25903acfcd3d4fd5bed31c2f50322928">
