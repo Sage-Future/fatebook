acd4215fed07500e8e20c3edbc4f474649335d1d
Daniel Gibbons <daniel@airmanual.co>
2024-12-22 16:18:10 -0300

    refactor normalizeOptions

--- lib/_utils_multiple-choice.ts
+++ lib/_utils_multiple-choice.ts
@@ -1,45 +1,40 @@
 import { OptionType } from "../components/predict-form/PredictProvider"
 
 export function normalizeOptionsToHundred(options: OptionType[]) {
-  const optionsWithForecasts = options.filter(
+  const optionsCopy = [...options]
+  const optionsWithForecasts = optionsCopy.filter(
     ({ forecast }) => forecast !== undefined && !Number.isNaN(forecast)
   ) as Array<OptionType & { forecast: number }>
-
-  const optionsWithoutForecasts = options.filter(
+  const optionsWithoutForecasts = optionsCopy.filter(
     ({ forecast }) => forecast === undefined || Number.isNaN(forecast)
   )
 
-  const allOptionsHaveForecasts = optionsWithForecasts.length === options.length
-  const normalizedOptions = [...options]
+  const allOptionsHaveForecasts = optionsWithForecasts.length === optionsCopy.length
+  const totalPercentage = optionsWithForecasts.reduce(
+    (acc, option) => acc + option.forecast,
+    0
+  )
 
   if (allOptionsHaveForecasts) {
-    const totalPercentage = optionsWithForecasts.reduce(
-      (acc, option) => acc + option.forecast,
-      0
-    )
     optionsWithForecasts.forEach((option, index) => {
-      const scaledValue = (option.forecast! * 100) / totalPercentage
-      normalizedOptions[index] = {
+      const scaledValue = (option.forecast * 100) / totalPercentage
+      optionsCopy[index] = {
         ...option,
         forecast: scaledValue
       }
     })
   } else {
-    const totalPercentage = optionsWithForecasts.reduce(
-      (sum, option) => sum + option.forecast,
-      0
-    )
     const remainingMass = Math.max(0, 100 - totalPercentage)
     const massPerOption = remainingMass / optionsWithoutForecasts.length
 
     optionsWithoutForecasts.forEach((option) => {
       const originalIndex = options.findIndex((o) => o === option)
-      normalizedOptions[originalIndex] = {
+      optionsCopy[originalIndex] = {
         ...option,
         forecast: massPerOption
       }
     })
   }
 
-  return normalizedOptions
+  return optionsCopy
 }
