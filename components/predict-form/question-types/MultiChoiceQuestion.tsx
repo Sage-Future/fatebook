45cf930fb08fe5fe07f35fa2192980a8e4114931
Daniel Gibbons <daniel@airmanual.co>
2024-12-22 15:38:29 -0300

    add basic sum to 100 logic

--- components/predict-form/question-types/MultiChoiceQuestion.tsx
+++ components/predict-form/question-types/MultiChoiceQuestion.tsx
@@ -8,6 +8,8 @@ import { PredictButton } from "../PredictButton"
 import { QuestionOption } from "../QuestionOption"
 import { ResolveBy } from "../ResolveBy"
 import { QuestionTypeProps } from "./question-types"
+import { OptionType } from "../PredictProvider"
+import { formatDecimalNicely } from "../../../lib/_utils_common"
 
 export default function MultiChoiceQuestion({
   small,
@@ -17,7 +19,7 @@ export default function MultiChoiceQuestion({
   onSubmit,
   highlightResolveBy,
 }: QuestionTypeProps) {
-  const { trigger, control } = useFormContext()
+  const { trigger, control, watch, setValue } = useFormContext()
 
   const MIN_OPTIONS = 2
   const MAX_OPTIONS = 100
@@ -40,6 +42,50 @@ export default function MultiChoiceQuestion({
     [remove],
   )
 
+  const options = watch("options") as OptionType[]
+  const normalizeToHundred = useCallback(() => {
+    const optionsWithForecasts = options.filter(
+      ({ forecast }) => forecast !== undefined && !Number.isNaN(forecast)
+    ) as Array<OptionType & { forecast: number }>
+
+    const optionsWithoutForecasts = options.filter(
+      ({ forecast }) => forecast === undefined || Number.isNaN(forecast)
+    )
+
+    const allOptionsHaveForecasts = optionsWithForecasts.length === options.length
+
+    if (allOptionsHaveForecasts) {
+      const totalPercentage = optionsWithForecasts.reduce(
+        (acc, option) => acc + option.forecast,
+        0,
+      )
+      optionsWithForecasts.forEach((option, index) => {
+        const scaledValue = (option.forecast! * 100) / totalPercentage
+        setValue(
+          `options.${index}.forecast`,
+          formatDecimalNicely(scaledValue, 0),
+        )
+      })
+    } else if (!allOptionsHaveForecasts) {
+      const totalPercentage = optionsWithForecasts.reduce(
+        (sum, option) => sum + option.forecast,
+        0,
+      )
+
+      if (totalPercentage > 100) return
+
+      const remainingMass = Math.max(0, 100 - totalPercentage)
+      const massPerOption = remainingMass / optionsWithoutForecasts.length
+      const roundedMassPerOption = formatDecimalNicely(massPerOption, 0)
+      optionsWithoutForecasts.forEach((option) => {
+        const originalIndex = options.findIndex((o) => o === option)
+        setValue(`options.${originalIndex}.forecast`, roundedMassPerOption)
+      })
+    }
+
+    void trigger("options")
+  }, [options, setValue, trigger])
+
   return (
     <div
       className={`flex flex-col gap-4 flex-wrap justify-between ${embedded ? "flex-col" : "flex-row"}`}
@@ -60,24 +106,34 @@ export default function MultiChoiceQuestion({
           />
         ))}
 
-        {fields.length < MAX_OPTIONS && (
-          <div className="flex flex-row justify-between items-center gap-2">
-            <div className="flex-grow">
+        <div className="flex flex-row justify-between items-center gap-2">
+          <div className="flex-grow flex gap-2">
+            <button
+              onClick={(e) => {
+                e.preventDefault()
+                addOption()
+              }}
+              className="btn btn-sm text-neutral-500"
+            >
+              <PlusIcon className="w-5 h-5" />
+              Add option
+            </button>
+
+            {
               <button
                 onClick={(e) => {
                   e.preventDefault()
-                  addOption()
+                  normalizeToHundred()
                 }}
                 className="btn btn-sm text-neutral-500"
               >
-                <PlusIcon className="w-5 h-5" />
-                Add option
+                Sum to 100%
               </button>
-            </div>
-            <div className=""></div>
-            <div className=""></div>
+            }
           </div>
-        )}
+          <div className=""></div>
+          <div className=""></div>
+        </div>
 
         <FormCheckbox
           onSubmit={onSubmit}
@@ -86,7 +142,7 @@ export default function MultiChoiceQuestion({
           helpText="If selected, you can resolve multiple options to YES. Otherwise, you can only resolve a single option to YES (and an OTHER option is added by default)."
           labelClassName="text-sm"
           onChange={() => {
-            void trigger("options") // needed because our superRefine rule for summing to 100% isn't automatically revalidated otherwise
+            void trigger("options")
           }}
         />
       </div>
